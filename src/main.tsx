b0fd709a04b797495a69f00bc67009d057323cfc
Aaron Tolentino <aarontolentinotrejo@gmail>
2026-02-02 20:09:38 -0800

    Correct auth

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,20 @@
-import '@taloon/nowpayments-components/styles';
-import './index.css';
+  import '@taloon/nowpayments-components/styles';
+  import './index.css';
 
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { NowPaymentsProvider } from '@taloon/nowpayments-components';
-// import BitcoinChipsProvider from './context/DataContext'
-import ReactDOM from 'react-dom/client';
+  import { GoogleOAuthProvider } from '@react-oauth/google';
+  import { NowPaymentsProvider } from '@taloon/nowpayments-components';
+  // import BitcoinChipsProvider from './context/DataContext'
+  import ReactDOM from 'react-dom/client';
 
-import App from './App.tsx';
-import { AuthProvider } from './hooks/useLogInHook.tsx';
+  import App from './App.tsx';
+  import { AuthProvider } from './hooks/useLogInHook.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <GoogleOAuthProvider clientId="266147379640-8h0mgp0hritd5kvo72dnfq3ljb2to02i.apps.googleusercontent.com">
-    {/* // <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}> */}
-    <AuthProvider>
-      <NowPaymentsProvider apiKey="6HWSMB8-2KH4AFP-KFXNNSF-NNQP9FT">
-        <App />
-      </NowPaymentsProvider>
-    </AuthProvider>
-  </GoogleOAuthProvider>
-);
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <AuthProvider>
+        <NowPaymentsProvider apiKey="6HWSMB8-2KH4AFP-KFXNNSF-NNQP9FT">
+          <App />
+        </NowPaymentsProvider>
+      </AuthProvider>
+    </GoogleOAuthProvider>
+  );
